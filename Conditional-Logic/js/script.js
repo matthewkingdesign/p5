9359690af1ab4e89968aef57d44a90c92510367b
matthewkingdesign <hello@matthewking.design>
2020-04-23 17:58:02 +0100

    added a simple if statement

--- Conditional-Logic/js/script.js
+++ Conditional-Logic/js/script.js
@@ -1,7 +1,13 @@
 function setup(){
   createCanvas(600,400);
-  background(100,100,100);
+  background(255,84,100);
 }
 function draw() {
+  noStroke();
+  fill(255,229,33);
 
+  if(mouseX > width/2){
+    fill(100);
+  }
+  ellipse(300,200,50,50);
 }
